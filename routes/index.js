434627c9e13002d4ebc638a5a6e4977b94263c4d
pranavkotwal <pkotwal24@gmail.com>
2023-03-05 00:01:42 +0530

    Add controlers and routers for users and posts

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,19 @@ const express = require('express');
 
 const router = express.Router()
 const homeController = require('../controllers/home_controller')
+
 console.log(`router loaded`)
+// From main index.js >> app.use('/') any requests that come from controllers 
+// would go to route/index.js and when we use route.use('/users....) >> 
+// it would redirect to user controllers
+
 
 router.get('/',homeController.home)
+//whenever the pattern of the path is for users it would be forwarded to users route
+router.use('/users',require(`./users`))
 
-router.get('/form/',homeController.form)
+// for any further routes, acces from here
+// router.use('/routerName', require('./routerFile))
+router.get('/posts',require(`./posts`))
 
 module.exports = router
