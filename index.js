537ac06c3c86559645310cc5ad85453a95a820b7
pranavkotwal <pkotwal24@gmail.com>
2023-03-11 22:12:57 +0530

    Setup Mongoose Database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const expressLayouts = require('express-ejs-layouts')
 const port = 8000;
+const db = require('./config/mongoose')
 
 app.use(expressLayouts)
 //extract style and scripts from sub pages into the layout
